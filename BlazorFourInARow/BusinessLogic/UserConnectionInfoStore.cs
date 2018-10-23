234e3701ae8a6ad488f0d8f5385937190fa4e819
James Schafer <james@jschafer.net>
2018-10-22 21:40:17 -0400

    Fix issue with Expired SignalR Connections

--- BlazorFourInARow/BusinessLogic/UserConnectionInfoStore.cs
+++ BlazorFourInARow/BusinessLogic/UserConnectionInfoStore.cs
@@ -2,7 +2,9 @@
 using BlazorFourInARow.Common.Models;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Blazor;
 
 namespace BlazorFourInARow.BusinessLogic
 {
@@ -10,17 +12,25 @@ namespace BlazorFourInARow.BusinessLogic
     {
         private readonly LocalStorage _localStorage;
         private readonly ILogger<UserConnectionInfoStore> _logger;
+        private readonly HttpClient _httpClient;
+        private readonly IServiceBaseUrlProvider _serviceBaseUrlProvider;
         const string KEY = "user-connection-info";
 
-        public UserConnectionInfoStore(LocalStorage localStorage, ILogger<UserConnectionInfoStore> logger)
+        public UserConnectionInfoStore(LocalStorage localStorage, ILogger<UserConnectionInfoStore> logger, HttpClient httpClient, IServiceBaseUrlProvider serviceBaseUrlProvider)
         {
             _localStorage = localStorage;
             _logger = logger;
+            _httpClient = httpClient;
+            _serviceBaseUrlProvider = serviceBaseUrlProvider;
         }
 
         public async Task<UserConnectionInfo> GetUserConnectionInfoAsync()
         {
             var userConnectionInfo = await _localStorage.GetItem<UserConnectionInfo>(KEY);
+
+            userConnectionInfo =
+                await _httpClient.PostJsonAsync<UserConnectionInfo>(
+                    $"{_serviceBaseUrlProvider.GetServiceBaseUrl()}/api/user", userConnectionInfo.User);
 
             _logger.LogInformation($"User Object Information.  IsNull = {null == userConnectionInfo}.  ID = {userConnectionInfo?.User?.UserId}");
 
